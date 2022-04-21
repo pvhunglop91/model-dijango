7db5b7ce8c48687554c9e58557023663235439b8
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2022-04-21 22:14:05 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,44 @@
-class Room(models.Model):
-    name = models.CharField(max_length=1000)
-class Message(models.Model):
-    value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
-    user = models.CharField(max_length=1000000)
-    room = models.CharField(max_length=1000000)
+from django.db import models
+from django.db.models import Q
+from django.core.urlresolvers import reverse
+
+
+        # -------------------------------------
+# bảng User
+class User(models.Model) :
+    user_name = models.CharField(max_length=1000)  
+    user_email = models.CharField(max_length=1000) 
+    user_passwords = models.CharField(max_length=1000)  
+    user_phone = models.CharField(max_length=1000)  
+    user_address =  models.TextField(max_length=500, blank=True, null=True)   
+   
+# bảng Products
+class Product(models.Model) :
+    categories_id = models.CharField(max_length=1000)
+    product_title = models.CharField(max_length=1000) 
+    product_price = models.PositiveIntegerField()
+    product_desc = models.TextField(max_length=500, blank=True, null=True)
+    product_images = models.ImageField(upload_to='products/', null=True)
+# bảng Categories
+class Categories(models.Model):
+    categories_title = models.CharField(max_length=200)
+
+# bảng Cart
+class Cart(models.Model):
+    product_id = models.CharField(max_length=1000)
+    user_id = models.CharField(max_length=1000)
+    quality = models.IntegerField(null=True, blank=True)
+
+# bảng  Order
+class Order(models.Model):
+    product_id = models.CharField(max_length=1000)
+    user_id = models.CharField(max_length=1000)
+    quality = models.IntegerField(null=True, blank=True)
+    ngaythang = models.DateTimeField(auto_now_add=True)
+    tinhtrang = models.CharField(max_length=1000)
+
+# bảng Contact
+class Contact(models.Model):
+    user_id = models.CharField(max_length=1000)
+    message = models.TextField(max_length=500, blank=True, null=True)
+    day = models.DateTimeField(auto_now_add=True)
